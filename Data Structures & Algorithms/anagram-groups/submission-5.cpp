18391b47d01cf9c357df14677f711ea92b39d081
MuhurthSR <146332138+MuhurthSR@users.noreply.github.com>
2026-09-05 16:46:02 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> tracker;
+
+        for(const string& s : strs){
+            string key(26,0);
+
+            for(char c : s){
+                key[c-'a']++;
+            }
+
+            tracker[key].push_back(s);
+        }
+
+        vector<vector<string>> res;
+
+        for(auto& pair : tracker){
+            res.push_back(pair.second);
+        }
+
+        return res;
+    }
+};
